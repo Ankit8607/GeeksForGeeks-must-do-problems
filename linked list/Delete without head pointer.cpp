1918bf79cc3c9e48d4a7671598d1722047c839f2
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-04 10:32:31 +0530

    Create Delete without head pointer.cpp

---
+++ linked list/Delete without head pointer.cpp
@@ -0,0 +1,13 @@
+// https://practice.geeksforgeeks.org/problems/delete-without-head-pointer/1
+
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+       del->data=del->next->data;
+       del->next=del->next->next;
+    }
+
+};
