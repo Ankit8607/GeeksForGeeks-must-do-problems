7dc0067e129f915ac53f5c3a456327f57509042f
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-06 17:21:12 +0530

    Create Party of Couples.cpp

---
+++ bit magic/Party of Couples.cpp
@@ -0,0 +1,10 @@
+// https://practice.geeksforgeeks.org/problems/alone-in-couple5507/1
+
+class Solution{
+    public:
+    int findSingle(int N, int arr[]){
+        int ans=0;
+        for(int i=0;i<N;i++) ans^=arr[i];
+        return ans;
+    }
+};
