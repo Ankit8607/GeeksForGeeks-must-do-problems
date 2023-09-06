a6e848f61255c7204813b12cb3c7aac48de012b9
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-06 16:14:48 +0530

    Create Find first set bit.cpp

---
+++ bit magic/Find first set bit.cpp
@@ -0,0 +1,12 @@
+// https://practice.geeksforgeeks.org/problems/find-first-set-bit-1587115620/1
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        if(n==0) return n;
+        return log2(n^(n-1))+1;
+    }
+};
